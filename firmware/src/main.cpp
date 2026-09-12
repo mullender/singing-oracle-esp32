8c8ba4478cc562f3d31c6133c52c6d499f80751c
mullender <matthijs.mullender@gmail.com>
2026-09-12 15:27:54 -0700

    Swap MIDI TX pin to G2 per bench test

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
-// Grove TX on AtomS3 Lite: TXD2 = G1 per m5stack_atoms3/pins_arduino.h
-static const int MIDI_TX_PIN = 1;
+// Grove TX to Unit Synth: G2 (bench-verified; TXD2 label in variant is misleading for this pairing)
+static const int MIDI_TX_PIN = 2;
 
 void setup() {
   Serial1.begin(31250, SERIAL_8N1, -1, MIDI_TX_PIN);
